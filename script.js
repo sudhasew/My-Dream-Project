b893fadcf5cfe05dcb17c6bd102a4291a52394ca
Chandu Valiveti <chanduvaliveti@Chandus-MBP.twmi.rr.com>
2021-12-05 21:02:08 -0500

    modified some stylings and changed some script

--- script.js
+++ script.js
@@ -110,22 +110,29 @@ function displayCart() {
   let cartItems = localStorage.getItem("productsInCart");
   cartItems = JSON.parse(cartItems);
   let productContainer = document.querySelector(".products");
+  let cartCost = localStorage.getItem("totalCost");
   if (cartItems && productContainer) {
     productContainer.innerHTML = "";
     Object.values(cartItems).map((item) => {
       productContainer.innerHTML += `<div class="product">
            <ion-icon name="close-circle-outline"></ion-icon>
-           <img src="./assets/${item.tag}.jpeg" width="100px">
+           <img src="./assets/${item.tag}.jpeg">
            <span>${item.name}</span> 
            </div>
-           <div class="price">${item.price}</div>
+           <div class="price">$${item.price}.00</div>
            <div class="quantity">
            <ion-icon name="arrow-dropleft-circle"></ion-icon>
            <span>${item.inCart}</span>
            <ion-icon name="arrow-dropright-circle"></ion-icon>
            </div>
+           <div class="total">
+           $${item.inCart * item.price}.00
+           </div>
            `;
     });
+    productContainer.innerHTML += `<div class="basketTotalContainer">
+    <h4 class="basketTotalTitle">Basket Total </h4>
+    <h4 class="basketTotal">$${cartCost}.00`;
   }
 }
 onLoadCartNumbers();
